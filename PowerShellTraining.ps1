baad2a0082c1b02c5f6ad9de1efd7151c32830ec
muhammad_umar <muhammad_umar@dell.com>
2016-10-10 13:21:40 -0500

    Powershell Training During the CBT Training
    CBT Nuggets - Windows PowerShell v2-v3-v4 Ultimate Training

---
+++ PowerShellTraining.ps1
@@ -0,0 +1,30 @@
+﻿#Powershell Logical Contructs using (if,elseif) and switch#
+
+#Using the if and elseif contruct
+
+<#
+$procs = Get-Process
+
+if ($procs.count -gt 1000) {
+    Write-Host "You have a lot of processes"
+} else {
+    Write-Host "less than 100 process"
+    }
+    #>
+
+
+#Using the SWITCH construct
+
+<#
+$name = Read-Host 'please enter the name of the server'
+
+Switch -Regex ($name) {
+
+"LON"   { Write-Host 'the server is located in London' 
+          }
+"Tokyo" { Write-Host 'the server is located in Japan' 
+          }
+}
+#>
+
+
